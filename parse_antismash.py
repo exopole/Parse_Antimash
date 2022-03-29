9fcf17de4c09a2df8e5163660d939f11226260c3
Exopole <alexandre.nicaise28@gmail.com>
2022-03-29 12:53:08 +0200

    Better parsing

--- parse_antismash.py
+++ parse_antismash.py
@@ -15,11 +15,21 @@ from Bio.SeqFeature import (
 )
 from collections import Counter
 
+geneFunctions = []
+geneQualifier = []
 
 class Gene:
-    def __init__(self, name, sequence):
+    Location: FeatureLocation
+
+    def __init__(self, name, sequence, function, location):
         self.Name = name
         self.Sequence = sequence
+        self.Function = function
+        self.Location = location
+
+    def tostring(self):
+        return self.Name + " " + str(self.Function) + " " + str(self.Location.start.position) + " : " + str(
+            self.Location.end.position)
 
 
 class Cluster:
@@ -82,48 +92,75 @@ def parse_id_gene(name):
         return output[1]
     return name
 
+
+def find_clusters_for_gene(gene, clusters):
+    """
+
+    @type clusters: Cluster[]
+    @type gene: Gene
+    """
+    i = 0
+    cluster = None
+    while i < len(clusters):
+        cluster = clusters[i]
+        # print(cluster.location.start + " <= " + gene.Location.start + " and " + cluster.location.end + " >= " + gene.Location.end)
+        if (cluster.location.start <= gene.Location.start and cluster.location.end >= gene.Location.end):
+            cluster.Genes.append(gene)
+            if cluster.Sp == "":
+                cluster.Sp = parse_id_gene(gene.Name)
+        i += 1
+
+
 def ParsingAndWriting(infile, outfile):
     o = open(outfile, "w")
 
     clusters = []
-    cluster = Cluster()
-
+    genes = []
 
     with open(infile) as handle:
         for record in SeqIO.parse(handle, format="gb"):
             for feature in record.features:
                 if feature.type == "cand_cluster":
-                    if cluster.Tag != "" and len(cluster.Genes) > 0:
-                        # print("In for => " + str(len(cluster.Genes)))
-                        clusters.append(cluster)
-                        cluster = Cluster()
-                        cluster.Genes = []
+                    # print("In for => " + str(len(cluster.Genes)))
+                    cluster = Cluster()
+                    cluster.Genes = []
+
+                    # print("Location cluster : " + str(feature.location.start))
 
                     product = feature.qualifiers["product"][0]
+                    # print(product)
                     cluster.Tag = product
                     cluster.location = feature.location
-                if feature.type == "CDS":
-                    if "gene_functions" in feature.qualifiers:
-                        if product in feature.qualifiers["gene_functions"][0]:
-                            if cluster.Sp == "" and "gene" in feature.qualifiers:
-                                cluster.Sp = parse_id_gene(feature.qualifiers["gene"][0])
-                                # print(feature.qualifiers["gene"])
-                                # print(len(cluster.Genes))
-                                gene = Gene(str(feature.qualifiers["gene"][0]), str(feature.qualifiers["translation"][0]))
-                                cluster.Genes.append(gene)
+                    clusters.append(cluster)
 
-            if cluster.Tag != "" and len(cluster.Genes) > 0:
-                # print("Appen after record ===> " + str(len(cluster.Genes)))
-                clusters.append(cluster)
-                cluster = Cluster()
-                cluster.Genes = []
+                if feature.type == "CDS":
+                    for qualifier in feature.qualifiers:
+                        if qualifier not in geneQualifier:
+                            geneQualifier.append(qualifier)
+
+                    name = str(feature.qualifiers["gene"][0])
+                    sequence = str(feature.qualifiers["translation"][0])
+                    function = ""
+
+                    if "gene_functions" in feature.qualifiers:
+                        function = feature.qualifiers["gene_functions"][0]
+                    elif "NRPS_PKS" in feature.qualifiers:
+                        function = feature.qualifiers["NRPS_PKS"]["type"]
+
+                    gene = Gene(name, sequence, function, feature.location)
+                    genes.append(gene)
+
+    for gene in genes:
+        find_clusters_for_gene(gene, clusters)
 
     toWrite = ""
     for item in clusters:
+        # print(str(len(item.Genes)))
         for gene in item.Genes:
-            toWrite += item.GetStringCommun() + str(gene.Name) + "\n"
-            print(">" + str(gene.Name) + "|" + item.Tag + "|" + item.Sp + "|" + str(item.location.start.position) + ":"
-                  + str(item.location.end.position))
+            if item.Tag in gene.Function:
+                toWrite += item.GetStringCommun() + str(gene.Name) + "\n"
+            # print(">" + str(gene.Name) + "|" + item.Tag + "|" + item.Sp + "|" + str(item.location.start.position) + ":"
+            #       + str(item.location.end.position))
 
     o.write(toWrite)
     o.close()
@@ -136,11 +173,17 @@ def main():
 
     if ARGS.directory is not None:
         path = ARGS.directory
-        path_to_file = glob.glob(path + '*.gbk')
+        path_to_file = glob.glob(path + '/*.gbk')
         for file in path_to_file:
             print(file)
             pathFileSplit = file.split('/')
-            nameFile = pathFileSplit[len(pathFileSplit)-1]
+            nameFile = pathFileSplit[len(pathFileSplit) - 1]
             ParsingAndWriting(file, nameFile[:-4])
+
+    # for qualifier in geneQualifier:
+    #     print(qualifier)
+
+    # for function in geneFunctions:
+    #     print(function)
 
 main()
